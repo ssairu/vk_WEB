04069ac3b018030a8231c2e13e4cab02a2ea06fd
sairu <a.penkin2018@yandex.ru>
2023-11-11 00:29:44 +0300

    fixes

--- WEB/WEB/urls.py
+++ WEB/WEB/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
+    path('', views.index),
+    path('base/', views.base),
+    path('question/', views.question),
     path('admin/', admin.site.urls),
 ]
