9289d7070537552bb93bfc2b4105d94d18906589
sairu <a.penkin2018@yandex.ru>
2023-11-15 17:48:27 +0300

    Add some pages

--- WEB/WEB/urls.py
+++ WEB/WEB/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('base/', views.base),
-    path('question/', views.question),
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('admin/', admin.site.urls),
 ]
--- WEB/app/views.py
+++ WEB/app/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 
-
 # Create your views here.
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Question {i}',
+        'content': f'A little questiion about something {i}',
+        'answers': range(5)
+    } for i in range(10)
+]
+
+
 def base(request):
     return render(request, 'base.html')
 
 
+def paginate(objects, page, per_page):
+    temppage = 1
+    if int == type(page):
+        temppage = page
+    paginator = Paginator(objects, per_page)
+    return paginator.page(temppage)
+
+
 def index(request):
-    return render(request, 'base.html')
+    page = request.GET.get('page', 1)
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page, 5)})
 
 
-def question(request):
-    return render(request, 'base.html')
+def question(request, question_id):
+    return render(request, 'question.html', {'question': QUESTIONS[question_id]})
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
